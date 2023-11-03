4c774e775aa9a603de2dd2b90169fc65ba6c561e
chanmyaithtoo <htoochan541@gmail.com>
2023-11-03 19:46:40 +1000

    Fuck u

--- utilities.js
+++ utilities.js
@@ -2,20 +2,24 @@ const path = require('path');
 const fs = require('fs').promises;
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
-const { v4: uuidv4 } = require('uuid');  // for generating unique file names
+const { v4: uuidv4 } = require('uuid');
 
 async function compressFiles(files) {
-    const tempFilePath = path.join(__dirname, `temp.7z`);
+    const tempDir = path.join(__dirname, 'CAB432_SQS'); // Ensuring files are saved in the correct directory
+    const tempFilePath = path.join(tempDir, `temp.7z`);
     const tempFiles = [];
 
     try {
+        // Ensure the directory exists
+        await fs.mkdir(tempDir, { recursive: true });
+
         // Save the files with unique names concurrently
         const writePromises = files.map(async file => {
-            const uniqueName = `${uuidv4()}_${file.originalname}`;
+            const uniqueName = path.join(tempDir, `${uuidv4()}_${file.originalname}`);
             await fs.writeFile(uniqueName, file.buffer);
             tempFiles.push(uniqueName);
         });
-        
+
         await Promise.all(writePromises);
 
         // Create a list of files to compress
@@ -28,6 +32,7 @@ async function compressFiles(files) {
         return compressedBuffer;
 
     } catch (err) {
+        console.error("Error compressing files:", err); // Added more detailed error logging
         throw err;
 
     } finally {
@@ -38,7 +43,6 @@ async function compressFiles(files) {
         ]);
     }
 }
-
 
 async function getUniqueFileName(desiredFileName) {
     let fileNameParts = desiredFileName.split('.');
@@ -51,16 +55,14 @@ async function getUniqueFileName(desiredFileName) {
 
 function format(timestamp) {
     const date = new Date(timestamp);
-    
-    // Extract day, month, year, hours, minutes, and seconds
+
     const day = String(date.getDate()).padStart(2, '0');
-    const month = String(date.getMonth() + 1).padStart(2, '0');  // Months are 0-based
+    const month = String(date.getMonth() + 1).padStart(2, '0');
     const year = date.getFullYear();
     const hours = String(date.getHours()).padStart(2, '0');
     const minutes = String(date.getMinutes()).padStart(2, '0');
     const seconds = String(date.getSeconds()).padStart(2, '0');
-    
-    // Format as dd/mm/yyyy hh:mm:ss
+
     return `${day},${month},${year} ${hours}:${minutes}:${seconds}`;
 }
 
