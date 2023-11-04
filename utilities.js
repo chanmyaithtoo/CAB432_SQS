2b560bf48432ec72cdd709112018393e497b1b19
chanmyaithtoo <htoochan541@gmail.com>
2023-11-04 21:52:29 +1000

    Change code

--- utilities.js
+++ utilities.js
@@ -1,19 +1,18 @@
+const { v4: uuidv4 } = require('uuid');
 const path = require('path');
 const fs = require('fs').promises;
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
-const { v4: uuidv4 } = require('uuid');
 
 async function compressFiles(files) {
-    const tempDir = path.join(__dirname, 'CAB432_SQS'); // Ensuring files are saved in the correct directory
-    const tempFilePath = path.join(tempDir, `temp.7z`);
+    const tempDir = path.join(__dirname, 'CAB432_SQS');
+    const archiveName = `archive_${uuidv4()}.7z`;
+    const tempFilePath = path.join(tempDir, archiveName);
     const tempFiles = [];
 
     try {
-        // Ensure the directory exists
         await fs.mkdir(tempDir, { recursive: true });
 
-        // Save the files with unique names concurrently
         const writePromises = files.map(async file => {
             const uniqueName = path.join(tempDir, `${uuidv4()}_${file.originalname}`);
             await fs.writeFile(uniqueName, file.buffer);
@@ -22,51 +21,41 @@ async function compressFiles(files) {
 
         await Promise.all(writePromises);
 
-        // Create a list of files to compress
-        const fileList = tempFiles.map(fileName => `"${fileName}"`).join(' ');
-
-        // Run 7zip command with maximum compression
-        await exec(`7z a -mx=9 ${tempFilePath} ${fileList}`);
+        const fileList = tempFiles.map(escapeFileName).join(' ');
+        const zipCommand = `7z a -mx=1 ${escapeFileName(tempFilePath)} ${fileList}`;
+        
+        console.log("Executing compression command:", zipCommand);
+        await exec(zipCommand);
 
         const compressedBuffer = await fs.readFile(tempFilePath);
         return compressedBuffer;
 
-    } catch (err) {
-        console.error("Error compressing files:", err); // Added more detailed error logging
-        throw err;
-
+    } catch (error) {
+        console.error(`Error during file compression:`, error);
+        throw error;
     } finally {
-        // Always cleanup, regardless of success or failure
-        await Promise.allSettled([
-            fs.unlink(tempFilePath),
-            ...tempFiles.map(tempFile => fs.unlink(tempFile))
-        ]);
+        const cleanupPromises = tempFiles.map(file => fs.unlink(file).catch(err => console.error(`Failed to delete temporary file:`, file, err)));
+        await Promise.all(cleanupPromises);
+
+        try {
+            await fs.unlink(tempFilePath);
+            console.log(`Deleted temporary archive: ${tempFilePath}`);
+        } catch (error) {
+            console.error(`Failed to delete temporary archive:`, tempFilePath, error);
+        }
     }
 }
 
-async function getUniqueFileName(desiredFileName) {
-    let fileNameParts = desiredFileName.split('.');
-    let baseName = fileNameParts[0];
-    let extension = fileNameParts[1];
-
-    let currentFileName = `${baseName}(${format(Date.now())}).${extension}`;
-    return currentFileName;
+function getUniqueFileName(desiredFileName) {
+    const timestamp = new Date().toISOString().replace(/[:.-]/g, '_');
+    return `${desiredFileName.replace(/\.[^/.]+$/, '')}_${timestamp}.${desiredFileName.split('.').pop()}`;
 }
 
-function format(timestamp) {
-    const date = new Date(timestamp);
-
-    const day = String(date.getDate()).padStart(2, '0');
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const year = date.getFullYear();
-    const hours = String(date.getHours()).padStart(2, '0');
-    const minutes = String(date.getMinutes()).padStart(2, '0');
-    const seconds = String(date.getSeconds()).padStart(2, '0');
-
-    return `${day},${month},${year} ${hours}:${minutes}:${seconds}`;
+function escapeFileName(filename) {
+    return `"${filename.replace(/(["\\$`])/g, '\\$1')}"`;
 }
 
 module.exports = {
     compressFiles,
-    getUniqueFileName,
-}
+    getUniqueFileName
+};
--- worker.js
+++ worker.js
@@ -1,134 +1,88 @@
+require('dotenv').config();
 const AWS = require('aws-sdk');
-const dotenv = require('dotenv');
 const { compressFiles, getUniqueFileName } = require("./utilities");
-dotenv.config();
 
 AWS.config.update({
   region: process.env.AWS_REGION,
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  sessionToken: process.env.AWS_SESSION_TOKEN
+  sessionToken: process.env.AWS_SESSION_TOKEN // Optional, only if necessary for your setup
 });
 
 const SQS = new AWS.SQS();
 const S3 = new AWS.S3();
 const QUEUE_URL = process.env.SQS_QUEUE_URL;
 
-async function processMessages() {
-  const messages = await SQS.receiveMessage({
-    QueueUrl: QUEUE_URL,
-    MaxNumberOfMessages: 5,
-    WaitTimeSeconds: 20,
-    VisibilityTimeout: 300 // 5 minutes
+async function fetchFileFromS3(filePath) {
+  console.log("Fetching file from S3 with path:", filePath);
+  const fileData = await S3.getObject({
+    Bucket: process.env.AWS_BUCKET,
+    Key: filePath
   }).promise();
+  return { originalname: filePath.split('/').pop(), buffer: fileData.Body };
+}
 
-  if (!messages.Messages) {
-    console.log("No messages to process");
-    return;
-  }
+async function processMessage(message) {
+  console.log("Received SQS Message:", message.Body);
+  const body = JSON.parse(message.Body);
 
-  for (let message of messages.Messages) {
-    console.log("Received SQS Message:", message.Body);  // Debugging line
-    const body = JSON.parse(message.Body);
+  // Fetch files from S3 concurrently
+  const fileBuffers = await Promise.all(body.files.map(fetchFileFromS3));
+
+  // Compress files
+  const compressedBuffer = await compressFiles(fileBuffers);
+  const dirName = `${body.username}_uploads`;
+  const uniqueFileName = getUniqueFileName(body.desiredFileName);
+
+  // Upload compressed file to S3
+  const uploadParams = {
+    Bucket: process.env.AWS_BUCKET,
+    Key: `${dirName}/${uniqueFileName}`,
+    Body: compressedBuffer
+  };
+  await S3.upload(uploadParams).promise();
+  console.log(`Processed and uploaded files for user: ${body.username}`);
+
+  // Delete the temporary files from S3
+  await Promise.all(body.files.map(filePath =>
+    S3.deleteObject({
+      Bucket: process.env.AWS_BUCKET,
+      Key: filePath
+    }).promise()
+  ));
+  console.log("Deleted temporary files from S3");
+
+  // Delete message from SQS
+  await SQS.deleteMessage({
+    QueueUrl: QUEUE_URL,
+    ReceiptHandle: message.ReceiptHandle
+  }).promise();
+  console.log("Deleted message from SQS");
+}
+
+async function pollSQSQueue() {
+  while (true) {
     try {
-      const fileBuffers = await Promise.all(body.files.map(async filePath => {
-        console.log("Trying to fetch file from S3 with path:", filePath);  // Debugging line
-        const fileData = await S3.getObject({
-          Bucket: process.env.AWS_BUCKET,
-          Key: filePath
-        }).promise();
-        return { originalname: filePath.split('/').pop(), buffer: fileData.Body };
-      }));
-
-      const compressedBuffer = await compressFiles(fileBuffers);
-      const dirName = `${body.username}_uploads`;
-      const uniqueFileName = await getUniqueFileName(body.desiredFileName);
-
-      const uploadParams = {
-        Bucket: process.env.AWS_BUCKET,
-        Key: `${dirName}/${uniqueFileName}`,
-        Body: compressedBuffer
-      };
-
-      await S3.upload(uploadParams).promise();
-      console.log(`Processed and uploaded files for user: ${body.username}`);
-
-      await Promise.all(body.files.map(filePath => {
-        return S3.deleteObject({
-          Bucket: process.env.AWS_BUCKET,
-          Key: filePath
-        }).promise();
-      }));
-      console.log("Deleted temporary files");
-
-      await SQS.deleteMessage({
+      const data = await SQS.receiveMessage({
         QueueUrl: QUEUE_URL,
-        ReceiptHandle: message.ReceiptHandle
+        MaxNumberOfMessages: 3,
+        WaitTimeSeconds: 20,
+        VisibilityTimeout: 300
       }).promise();
-      console.log("Deleted message from SQS");
 
+      const { Messages } = data;
+      if (Messages && Messages.length > 0) {
+        // Process messages concurrently
+        await Promise.all(Messages.map(processMessage));
+      } else {
+        console.log("No messages to process. Waiting for new messages...");
+      }
     } catch (error) {
-      console.error("Error processing message:", error.message); // Modified error logging
+      console.error("Error during SQS polling and message processing:", error);
+      // Implement exponential backoff or delay as needed before the next poll
+      await new Promise(resolve => setTimeout(resolve, 10000)); // 10 seconds delay
     }
   }
 }
 
-
-// async function processMessages() {
-//   const messages = await SQS.receiveMessage({
-//     QueueUrl: QUEUE_URL,
-//     MaxNumberOfMessages: 5,
-//     WaitTimeSeconds: 20
-//   }).promise();
-
-//   if (!messages.Messages) {
-//     console.log("No messages to process");
-//     return;
-//   }
-
-//   for (let message of messages.Messages) {
-//     const body = JSON.parse(message.Body);
-//     try {
-//       const fileBuffers = await Promise.all(body.files.map(async filePath => {
-//         const fileData = await S3.getObject({
-//           Bucket: process.env.AWS_BUCKET,
-//           Key: filePath
-//         }).promise();
-//         return { originalname: filePath.split('/').pop(), buffer: fileData.Body };
-//       }));
-
-//       const compressedBuffer = await compressFiles(fileBuffers);
-//       const dirName = `${body.username}_uploads`;
-//       const uniqueFileName = await getUniqueFileName(body.desiredFileName);
-
-//       const uploadParams = {
-//         Bucket: process.env.AWS_BUCKET,
-//         Key: `${dirName}/${uniqueFileName}`,
-//         Body: compressedBuffer
-//       };
-
-//       await S3.upload(uploadParams).promise();
-//       console.log(`Processed and uploaded files for user: ${body.username}`);
-
-//       await Promise.all(body.files.map(filePath => {
-//         return S3.deleteObject({
-//           Bucket: process.env.AWS_BUCKET,
-//           Key: filePath
-//         }).promise();
-//       }));
-//       console.log("Deleted temporary files");
-
-//       await SQS.deleteMessage({
-//         QueueUrl: QUEUE_URL,
-//         ReceiptHandle: message.ReceiptHandle
-//       }).promise();
-//       console.log("Deleted message from SQS");
-
-//     } catch (error) {
-//       console.error("Error processing message:", error);
-//       // Not deleting the message here ensures it gets reprocessed.
-//     }
-//   }
-// }
-
-setInterval(processMessages, 30000); // Poll every 30 seconds.
+pollSQSQueue();
